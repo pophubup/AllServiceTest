8cb16fc9802fdc5198a61defcd3414a2062105ae
Goodarc\Yukai <yuki@goodarc.com>
2021-09-06 16:33:58 +0800

    test

--- haha/Startup.cs
+++ haha/Startup.cs
@@ -87,7 +87,7 @@ namespace haha
             app.UseSwagger();
 
             app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "myDotNet development");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API_Resources");
                 c.RoutePrefix = string.Empty;
             });
            
