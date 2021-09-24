d061671befff8ff7c4a9afba18ffd66a0bbe6ec3
Goodarc\Yukai <yuki@goodarc.com>
2021-09-24 13:37:17 +0800

    tets

--- zAutoMapperRepository/AutoMapperService.cs
+++ zAutoMapperRepository/AutoMapperService.cs
@@ -13,39 +13,37 @@ namespace zAutoMapperRepository
     {
         public static IServiceCollection AddAutoMapperService(this IServiceCollection service)
         {
-            service.AddAutoMapper(cfg => {
-                cfg.AddProfile<ContainerMapperProfile>();
-                cfg.AddProfile<ProductMapperProfile>();
-                cfg.AddProfile<CategoryMappProfile>();
+            service.AddAutoMapper(
+            AppDomain.CurrentDomain.GetAssemblies(),
+            serviceLifetime: ServiceLifetime.Singleton);
 
-            },AppDomain.CurrentDomain.GetAssemblies(),serviceLifetime: ServiceLifetime.Singleton);
-         
-            var builder = service.BuildServiceProvider();
-            List<MyModel0> myModel0s = new List<MyModel0>();
-            for (int i = 0; i < 10; i++)
-            {
-                MyModel0 myModel0 = new MyModel0()
-                {
-                    Id = i,
-                    products = Enumerable.Range(1, 10).Select(x => new Product
-                    {
-                        Id = x,
-                        Name = x.ToString(),
-                        description = x.ToString() + x.ToString(),
-                        categories = Enumerable.Range(1,10).Select(y => new Category() { 
-                          Id = y,
-                           Name = y.ToString() + y.ToString() + y.ToString()
-                        }).ToList(),
-                        price = x * 10000
-                    }).ToList()
+            //var builder = service.BuildServiceProvider();
+            //List<MyModel0> myModel0s = new List<MyModel0>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    MyModel0 myModel0 = new MyModel0()
+            //    {
+            //        Id = i,
+            //        products = Enumerable.Range(1, 10).Select(x => new Product
+            //        {
+            //            Id = x,
+            //            Name = x.ToString(),
+            //            description = x.ToString() + x.ToString(),
+            //            categories = Enumerable.Range(1, 10).Select(y => new Category()
+            //            {
+            //                Id = y,
+            //                Name = y.ToString() + y.ToString() + y.ToString()
+            //            }).ToList(),
+            //            price = x * 10000
+            //        }).ToList()
 
-                };
-                myModel0s.Add(myModel0);
-            }
+            //    };
+            //    myModel0s.Add(myModel0);
+            //}
 
-            List<Container> containers = builder.GetService<IMapper>().Map<List<Container>>(myModel0s);
+            //List<Container> containers = builder.GetService<IMapper>().Map<List<Container>>(myModel0s);
 
-            List<MyModel0> containers2 = builder.GetService<IMapper>().Map<List<MyModel0>>(containers);
+            //List<MyModel0> containers2 = builder.GetService<IMapper>().Map<List<MyModel0>>(containers);
             return service;
         }
     }
